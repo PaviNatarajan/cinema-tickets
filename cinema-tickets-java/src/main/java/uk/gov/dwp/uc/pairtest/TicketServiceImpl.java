1ab4674282099aa0a57122a4acd2d748a72b9cc2
PaviNatarajan <pavinatarajan95sa@gmail.com>
2026-04-15 15:12:29 +0530

    Adding TicketServiceImpl with validation and Junit Tests

--- cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
+++ cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
@@ -2,15 +2,74 @@ package uk.gov.dwp.uc.pairtest;
 
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
 import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
+import thirdparty.paymentgateway.TicketPaymentServiceImpl;
+import thirdparty.seatbooking.SeatReservationServiceImpl;
+
 
 public class TicketServiceImpl implements TicketService {
     /**
      * Should only have private methods other than the one below.
      */
+   private final TicketPaymentServiceImpl paymentService;
+   private final SeatReservationServiceImpl seatReservationService;
+
+    public TicketServiceImpl() {
+        this.paymentService = new TicketPaymentServiceImpl();
+        this.seatReservationService = new SeatReservationServiceImpl();
+    }
 
     @Override
     public void purchaseTickets(Long accountId, TicketTypeRequest... ticketTypeRequests) throws InvalidPurchaseException {
 
+        validateAccount(accountId);
+
+        int adultCount = 0;
+        int childCount = 0;
+        int infantCount = 0;
+
+        for (TicketTypeRequest request : ticketTypeRequests) {
+            switch (request.getTicketType()) {
+                case ADULT:
+                    adultCount += request.getNoOfTickets();
+                    break;
+                case CHILD:
+                    childCount += request.getNoOfTickets();
+                    break;
+                case INFANT:
+                    infantCount += request.getNoOfTickets();
+                    break;
+            }
+        }
+        int totalTickets = adultCount + childCount + infantCount;
+
+        validateBusinessRules(adultCount, childCount, infantCount, totalTickets);
+
+        int totalAmount = (adultCount * 25) + (childCount * 15);
+        int totalSeats = adultCount + childCount;
+
+        paymentService.makePayment(accountId, totalAmount);
+        seatReservationService.reserveSeat(accountId, totalSeats);
     }
 
+    private void validateAccount(long accountId) {
+        if (accountId <= 0) {
+            throw new IllegalArgumentException("Invalid account ID");
+        }
+    }
+
+    private void validateBusinessRules(int adult, int child, int infant, int total) {
+
+        if (total == 0) {
+            throw new IllegalArgumentException("No tickets requested");
+        }
+        if (total > 25) {
+            throw new IllegalArgumentException("Max 25 tickets allowed");
+        }
+        if (adult == 0 && (child > 0 || infant > 0)) {
+            throw new IllegalArgumentException("Child or Infant cannot be purchased without Adult");
+        }
+        if (infant > adult) {
+            throw new IllegalArgumentException("Infants must sit on an adult lap");
+        }
+    }
 }
---
+++ cinema-tickets-java/src/test/java/uk/gov/dwp/uc/pairtest/TicketServiceImplTest.java
@@ -0,0 +1,44 @@
+package uk.gov.dwp.uc.pairtest;
+
+import org.junit.jupiter.api.Test;
+import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
+import static org.junit.jupiter.api.Assertions.*;
+import uk.gov.dwp.uc.pairtest.TicketServiceImpl;
+
+class TicketServiceImplTest {
+
+    private final TicketServiceImpl service = new TicketServiceImpl();
+
+    // ---------------- VALID CASE ----------------
+    @Test
+    void shouldAllowValidAdultChildPurchase() {
+        assertDoesNotThrow(() ->
+                service.purchaseTickets(
+                        1L,
+                        new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 2),
+                        new TicketTypeRequest(TicketTypeRequest.Type.CHILD, 1)
+                )
+        );
+    }
+
+    // ---------------- INVALID SCENARIOS ----------------
+    @Test
+    void shouldRejectInvalidAccountId() {
+        assertThrows(IllegalArgumentException.class, () ->
+                service.purchaseTickets(
+                        0L,
+                        new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 1)
+                )
+        );
+    }
+
+    @Test
+    void shouldRejectChildTicketWithoutAdult() {
+        assertThrows(IllegalArgumentException.class, () ->
+                service.purchaseTickets(
+                        1L,
+                        new TicketTypeRequest(TicketTypeRequest.Type.CHILD, 1)
+                )
+        );
+    }
+}
